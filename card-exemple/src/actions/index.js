35608dfe7f91dbe2aba5beb75c5c3e1fb96b8268
Ruben <ruben.soufir@gmail.com>
2018-12-19 23:24:47 +0100

    avancée sur jeu de la bataille

--- card-exemple/src/actions/index.js
+++ card-exemple/src/actions/index.js
@@ -53,11 +53,120 @@ export function loadPile1(deck_id){
     }
 }
 
+function addInPile1(deck_id, cards){
+    return(dispatch)=>{
+        return axios.get("https://deckofcardsapi.com/api/deck/" + deck_id + "/pile/playerone/add/?cards="+cards).then((response)=>{
+            dispatch(changePile1(response.data.piles.playerone.remaining));
+            dispatch(checkIfWinner(response.data.piles, response.data.remaining));
+        })
+    }
+} 
+function addInPile2(deck_id, cards){
+    return(dispatch)=>{
+        return axios.get("https://deckofcardsapi.com/api/deck/" + deck_id + "/pile/playertwo/add/?cards="+cards).then((response)=>{
+            dispatch(changePile2(response.data.piles.playertwo.remaining));
+            dispatch(checkIfWinner(response.data.piles, response.data.remaining));
+        })
+    }
+}
+
+export function addPile(cardPlayer1, cardPlayer2, deck_id){
+    const cardPlayer1Symbol = cardPlayer1.substring(
+        cardPlayer1.lastIndexOf("/") + 1, 
+        cardPlayer1.lastIndexOf(".")
+    )
+    const cardPlayer2Symbol = cardPlayer2.substring(
+        cardPlayer2.lastIndexOf("/") + 1, 
+        cardPlayer2.lastIndexOf(".")
+    )
+    cardPlayer1 = cardPlayer1.substring(
+        cardPlayer1.lastIndexOf("/") + 1, 
+        cardPlayer1.lastIndexOf(".") - 1
+    )
+    cardPlayer2 = cardPlayer2.substring(
+        cardPlayer2.lastIndexOf("/") + 1, 
+        cardPlayer2.lastIndexOf(".") - 1
+    )
+
+    const cardsTirees = cardPlayer1Symbol+','+cardPlayer2Symbol;
+    const array = ['0','J', 'Q', 'K', 'A'];
+    if(cardPlayer1 === cardPlayer2){
+        // égalité
+        console.log(cardPlayer1Symbol);
+        console.log(cardPlayer2Symbol);
+        return (dispatch)=>{
+            dispatch(addInPile1(deck_id, cardPlayer1Symbol))
+            dispatch(addInPile2(deck_id, cardPlayer2Symbol))
+        };
+    }
+    if(!arrayContains(cardPlayer1, array) && !arrayContains(cardPlayer2, array)){
+        if(cardPlayer1 > cardPlayer2){
+            // joueur 1 gagne
+            return (dispatch)=>{return dispatch(addInPile1(deck_id, cardsTirees))};
+        }else if(cardPlayer1 < cardPlayer2){
+            // joueur 2 gagne
+            return (dispatch)=>{return dispatch(addInPile2(deck_id, cardsTirees))};
+        }
+    }else if(!arrayContains(cardPlayer1, array) && arrayContains(cardPlayer2, array)){
+        // joueur 2 gagne
+        return (dispatch)=>{return dispatch(addInPile2(deck_id, cardsTirees))};
+    }else if(arrayContains(cardPlayer1, array) && !arrayContains(cardPlayer2, array)){
+        // joueur 1 gagne
+        return (dispatch)=>{return dispatch(addInPile1(deck_id, cardsTirees))};
+    }else if(arrayContains(cardPlayer1, array) && arrayContains(cardPlayer2, array)){
+        if(array.indexOf(cardPlayer1) > array.indexOf(cardPlayer2)){
+            // joueur 1 gagne
+            return (dispatch)=>{return dispatch(addInPile1(deck_id, cardsTirees))};
+        }else if(array.indexOf(cardPlayer1) < array.indexOf(cardPlayer2)){
+            // joueur 2 gagne
+            return (dispatch)=>{return dispatch(addInPile2(deck_id, cardsTirees))};
+        }
+    }
+}
 export function changePile1(pile1){
     return{
         type:"CHANGE_PILE1",
         pile1:pile1
     }
+}
+
+export function checkIfWinner(piles, remaining){
+    let playerOneWinnerOrNot = false;
+    let playerTwoWinnerOrNot = false;
+    if(remaining === 0){
+        if(piles.playerone.remaining > piles.playertwo.remaining){
+            // joueur 1 gagne
+            playerOneWinnerOrNot = true;
+        }else if(piles.playerone.remaining < piles.playertwo.remaining){
+            // joueur 2 gagne
+            playerTwoWinnerOrNot = true;
+        }
+    }
+    console.log(playerOneWinnerOrNot);
+    console.log(playerTwoWinnerOrNot);
+    return (dispatch)=>{return dispatch(checkWin(playerOneWinnerOrNot, playerTwoWinnerOrNot))};
+}
+
+export function checkWin(player1Win, player2Wiin){
+    return{
+        type:"WHAT_PLAYER_WIN",
+        player1Win:player1Win,
+        player2Wiin:player2Wiin
+    }
+}
+
+// export function addPileAction(cardPlayer1, cardPlayer2){
+    
+//     return{
+//         type:"BATAILLE",
+//         cardPlayer1:cardPlayer1,
+//         cardPlayer2:cardPlayer2
+//     }
+// }
+
+function arrayContains(needle, arrhaystack)
+{
+    return (arrhaystack.indexOf(needle) > -1);
 }
 
 export function loadPile2(deck_id){
--- card-exemple/src/component/box.js
+++ card-exemple/src/component/box.js
@@ -4,7 +4,6 @@ class Box extends React.Component {
     render() {
         return (
             <div className="wrapper">
-
                 <div className="loadDeck">
                     <div className="new-deck">{this.props.deck}</div>
                     <button onClick={()=>{this.props.handleClickLoadDeck()}}>Charger un nouveau deck</button>
@@ -19,11 +18,18 @@ class Box extends React.Component {
                 </div>
                 <div className="oneCart">
                     <p>Nombre de cartes de la pile : <br/> {this.props.pile1}</p>
-                    <button onClick={()=>{this.props.handleClick3(this.props.deck)}}>Pile 1</button>
+                    {/* <button onClick={()=>{this.props.handleClick3(this.props.deck)}}>Pile 1</button> */}
                 </div>
                 <div className="oneCart">
                     <p>Nombre de cartes de la pile : <br/> {this.props.pile2}</p>
-                    <button onClick={()=>{this.props.handleClick4(this.props.deck)}}>Pile 2</button>
+                    {/* <button onClick={()=>{this.props.handleClick4(this.props.deck)}}>Pile 2</button> */}
+                </div>
+                <div className="oneCart">
+                    <button onClick={()=>{this.props.handleClick5(this.props.card1, this.props.card2, this.props.deck)}}>Bataille</button>
+                </div>
+                <div className="oneCart">
+                    {this.props.player1Win === true && <p>Joueur 1 gagne la partie</p>}
+                    {this.props.player2Win === true && <p>Joueur 2 gagne la partie</p>}
                 </div>
             </div>
         )
